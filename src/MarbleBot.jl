46e8ea289a70e23887c59287cdb34f8aa494eb86
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-06-04 14:24:24 +0900

    Add version info into input file (#130)

--- src/MarbleBot.jl
+++ src/MarbleBot.jl
@@ -11,12 +11,20 @@ using Serialization
 
 using Base: @_propagate_inbounds_meta, @_inline_meta
 
+const PACKAGE_VERSION = let
+    project = TOML.parsefile(joinpath(pkgdir(@__MODULE__), "Project.toml"))
+    VersionNumber(project["version"])
+end
+
+include("compat.jl")
+
 include("input.jl")
 include("methods.jl")
 include("dem.jl")
 include("PenetrateIntoGround.jl")
 include("FreeRun.jl")
 
+# for PackageCompiler
 function julia_main()::Cint
     if isempty(ARGS)
         inputtoml = "input.toml"
@@ -30,6 +38,22 @@ function julia_main()::Cint
         return 1
     end
     return 0
+end
+
+##################
+# main functions #
+##################
+
+# helpers
+commas(num::Integer) = replace(string(num), r"(?<=[0-9])(?=(?:[0-9]{3})+(?![0-9]))" => ",")
+function replace_version(toml::AbstractString, version::VersionNumber)
+    toml_ver = TOML.parse(toml)["version"]
+    lines = split(toml, '\n')
+    n = findfirst(lines) do line
+        startswith(replace(line, " " => ""), "version=\"$toml_ver\"")
+    end
+    lines[n] = "version = \"$version\" # <\"$toml_ver\" is replaced by MarbleBot>"
+    join(lines, '\n')
 end
 
 function main(toml::AbstractString; kwargs...)
@@ -65,17 +89,13 @@ function main_tomlstring(toml::AbstractString; injection::Module = Module(), pro
     input.Injection = injection
     mkpath(input.Output.directory)
     if input.Output.copy_inputfile
+        toml = replace_version(toml, PACKAGE_VERSION)
         write(joinpath(input.Output.directory, "input.toml"), toml)
     end
     main(input, input.Phase)
 end
 
-commas(num::Integer) = replace(string(num), r"(?<=[0-9])(?=(?:[0-9]{3})+(?![0-9]))" => ",")
-function main(input::Input, phase::Input_Phase, (t, grid, gridstate, pointstate, data...) = initialize(input, phase))
-    println("Points: ", commas(length(pointstate)))
-    @eval $input.General.type.main($input, $phase, $t, $grid, $gridstate, $pointstate, $data...)
-end
-
+# main phases
 function main(input::Input, phases::Vector{Input_Phase})
     outdir = input.Output.directory
     t, grid, gridstate, pointstate, rigidbodies, data... = initialize(input, first(phases))
@@ -85,6 +105,16 @@ function main(input::Input, phases::Vector{Input_Phase})
         t = main(input, phases[i], (t, grid, gridstate, pointstate, rigidbodies, data...))
     end
 end
+
+# main for each phase
+function main(input::Input, phase::Input_Phase, (t, grid, gridstate, pointstate, data...) = initialize(input, phase))
+    println("Points: ", commas(length(pointstate)))
+    @eval $input.General.type.main($input, $phase, $t, $grid, $gridstate, $pointstate, $data...)
+end
+
+############################
+# initialize/reinitialize! #
+############################
 
 function initialize(input::Input, phase::Input_Phase)
     if isempty(phase.restart)
